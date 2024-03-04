7f0b3f7d9ac5d7a95bfa5e486dfaf77d962f4039
Andre Ruan <andreruan80@gmail.com>
2024-03-04 15:02:33 -0300

    FIRST M-V-C

---
+++ SalesWebMCV/Controllers/DepartmentsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SalesWebMCV.Models;
+
+namespace SalesWebMCV.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> dep = new List<Department>();
+            dep.Add(new Department { Id = 1, Name = "Eletronics" });
+            dep.Add(new Department { Id = 2, Name = "Fashion" });
+            return View(dep);
+        }
+    }
+}
--- SalesWebMCV/Controllers/HomeController.cs
+++ SalesWebMCV/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using SalesWebMCV.Models;
+using SalesWebMCV.Models.ViewModels;
 
 namespace SalesWebMCV.Controllers
 {
